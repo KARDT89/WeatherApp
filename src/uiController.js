c1e371ce561e55bc0d8a5579215201f62a34b962
dt89mint <dt89@mint.com>
2025-06-29 01:02:34 +0530

    add simple ui

--- src/uiController.js
+++ src/uiController.js
@@ -5,19 +5,42 @@ const form = document.getElementById("weather-form");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const location = e.target["location"].value;
-  const data = await weatherSearch(location);
-  showWeather(data)
-//   console.log(data);
-  
+  const weatherData = await weatherSearch(location);
+  const iconData = await IconSearch(weatherData.iconName);
+  showWeather(weatherData, iconData);
 });
 
-function showWeather(data) {
-  const currentTemparature = data.currentConditions.temp
-
+function showWeather(weatherData, iconData) {
+  const { currentTemperature, iconName, description } = weatherData;
+  const { imgURL } = iconData;
   const h1 = document.createElement("h1");
+  const h2 = document.createElement("h2");
   const content = document.getElementById("content");
-  h1.innerHTML = `${currentTemparature}`;
-  console.log(h1);
+  const vibe = document.getElementById("vibe");
+  const img = document.createElement("img");
+  const card = document.createElement("div");
+  const weatherIcon = document.createElement("img");
 
-  content.appendChild(h1);
+  content.innerHTML = "";
+  vibe.innerHTML = ""
+
+  h1.classList.add('current-temperature')
+  h2.classList.add('weather-description')
+  img.classList.add('weather-gif')
+  weatherIcon.classList.add('weather-icon')
+  card.classList.add('card')
+
+
+  h1.innerHTML = `${currentTemperature}`;
+  h2.innerHTML = `${description}`;
+  img.src = imgURL;
+  weatherIcon.src = `/icons/${iconName}.svg`;
+
+
+  card.appendChild(h1);
+  card.appendChild(weatherIcon);
+  card.appendChild(h2);
+
+  content.appendChild(card)
+  vibe.appendChild(img);
 }
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,3 +1,5 @@
+const GIPHY = process.env.GIPHY_API_KEY;
+
 export async function weatherSearch(location) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.WEATHER_API_KEY}`;
   try {
@@ -6,27 +8,31 @@ export async function weatherSearch(location) {
       throw new Error(`Response status: ${response.status}`);
     }
 
+    let data = {};
     const json = await response.json();
-    return json
-    
+    const { description } = json;
+    data.currentTemperature = json.currentConditions.temp;
+    data.iconName = json.currentConditions.icon;
+    data.description = description;
 
+    return data;
   } catch (error) {
     console.error(error.message);
   }
 }
 
 export async function IconSearch(icon) {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.WEATHER_API_KEY}`;
+  const url = `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY}&s=${icon}`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
+    let data = {};
     const json = await response.json();
-    return json
-    
+    data.imgURL = json.data.images["original"].url;
 
+    return data;
   } catch (error) {
     console.error(error.message);
   }
